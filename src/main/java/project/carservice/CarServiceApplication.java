c961109c72e65df32f4a89f2942e317474cccf4c
DKKolev <121520125+mk6300@users.noreply.github.com>
2024-06-12 20:46:04 +0300

    initial commit

---
+++ src/main/java/project/carservice/CarServiceApplication.java
@@ -0,0 +1,13 @@
+package project.carservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarServiceApplication.class, args);
+	}
+
+}
