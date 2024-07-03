7f6706d238df28489eaa213b3203f510a2945a63
DKKolev <121520125+mk6300@users.noreply.github.com>
2024-07-03 13:28:58 +0300

    changes in index.html

--- src/main/java/project/carservice/config/SecurityConfig.java
+++ src/main/java/project/carservice/config/SecurityConfig.java
@@ -18,41 +18,45 @@ public class SecurityConfig {
         return httpSecurity
                 .authorizeHttpRequests(
                         // Setup which URL-s are available to who
-                        authorizeRequests ->
-                                authorizeRequests
-                                        // all static resources to "common locations" (css, images, js) are available to anyone
-                                        .requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll()
-                                        // some more resources for all users
-                                        .requestMatchers("/", "/users/login", "/users/register", "/about").permitAll()
-                                        // all other URL-s should be authenticated.
-                                        .anyRequest()
-                                        .authenticated()
+                        authorizeRequests -> {
+                            authorizeRequests
+                                    // all static resources to "common locations" (css, images, js) are available to anyone
+                                    .requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll()
+                                    // some more resources for all users
+                                    .requestMatchers("/", "/users/login", "/users/register").permitAll()
+                                    // all other URL-s should be authenticated.
+                                    .anyRequest()
+                                    .authenticated();
+                        }
                 )
-                .formLogin(formLogin ->
-                        formLogin
-                                // Where is our custom login form?
-                                .loginPage("/users/login")
-                                // what is the name of the username parameter in the Login POST request?
-                                .usernameParameter("email")
-                                // what is the name of the password parameter in the Login POST request?
-                                .passwordParameter("password")
-                                // What will happen if the login is successful
-                                .defaultSuccessUrl("/home", true)
-                                // What will happen if the login fails
-                                .failureForwardUrl("/users/login-error")
+                .formLogin(formLogin -> {
+                            formLogin
+                                    // Where is our custom login form?
+                                    .loginPage("/users/login")
+                                    // what is the name of the username parameter in the Login POST request?
+                                    .usernameParameter("email")
+                                    // what is the name of the password parameter in the Login POST request?
+                                    .passwordParameter("password")
+                                    // What will happen if the login is successful
+                                    .defaultSuccessUrl("/home", true)
+                                    // What will happen if the login fails
+                                    .failureForwardUrl("/users/login-error");
+                        }
                 )
                 .logout(
-                        logout ->
-                                logout
-                                        // what is the logout URL?
-                                        .logoutUrl("/users/logout")
-                                        // Where to go after successful logout?
-                                        .logoutSuccessUrl("/")
-                                        // invalidate the session after logout.
-                                        .invalidateHttpSession(true)
+                        logout -> {
+                            logout
+                                    // what is the logout URL?
+                                    .logoutUrl("/users/logout")
+                                    // Where to go after successful logout?
+                                    .logoutSuccessUrl("/")
+                                    // invalidate the session after logout.
+                                    .invalidateHttpSession(true);
+                        }
                 )
                 .build();
     }
+
     @Bean
     public CarUserDetailService userDetailsService(UserRepository userRepository) {
         return new CarUserDetailService(userRepository);
