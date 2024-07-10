529cb9911cde03d433dee6e77217448982dcda99
DKKolev <121520125+mk6300@users.noreply.github.com>
2024-07-10 13:47:46 +0300

    add order html

--- src/main/java/project/carservice/service/OrderServiceImpl.java
+++ src/main/java/project/carservice/service/OrderServiceImpl.java
@@ -1,8 +1,23 @@
 package project.carservice.service;
 
+import org.modelmapper.ModelMapper;
 import project.carservice.model.dto.AddOrderDTO;
+import project.carservice.repository.OrderRepository;
 
 public class OrderServiceImpl implements OrderService{
+
+    private final OrderRepository orderRepository;
+    private final ModelMapper modelMapper;
+
+    private final UserService userService;
+
+    public OrderServiceImpl(OrderRepository orderRepository, ModelMapper modelMapper, UserService userService) {
+        this.orderRepository = orderRepository;
+        this.modelMapper = modelMapper;
+        this.userService = userService;
+    }
+
+
     @Override
     public void addOrder(AddOrderDTO addOrderDTO) {
 
